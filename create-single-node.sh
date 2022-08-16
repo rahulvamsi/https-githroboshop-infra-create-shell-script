7c285ca8035566017d13d0a45c28514b75a32f9a
Raghu K <raghuk.vit@gmail.com>
2022-08-16 08:53:13 +0530

    Update roboshop-infra-create-shell-script/

---
+++ create-single-node.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##### Change these values ###
+ZONE_ID="Z05563911B5TPM0CPTZND"
+SG_NAME="allow-all"
+ENV="dev"
+#############################
+
+
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t2.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+}
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+if [ -z "$1" ]; then
+  echo "Input the node name"
+  exit 1
+else
+  COMPONENT=$1
+  create_ec2
+fi
+
+
+
